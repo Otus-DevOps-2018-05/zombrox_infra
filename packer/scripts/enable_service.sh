bfa1182e8365e65ae692fef3a008e4276ecfde82
Oleg Klimin <oleg.v.klimin@gmail.com>
2018-07-10 20:44:58 +0300

    made full homework

---
+++ packer/scripts/enable_service.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cat  <<EOF > puma.service
+[Unit]
+Description=Puma
+Requires=network-online.target
+After=network.target
+[Service]
+WorkingDirectory=/home/appuser/reddit
+ExecStart=/usr/local/bin/puma
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo mv puma.service /etc/systemd/system/
+sudo chown  root:root /etc/systemd/system/puma.service
+sudo  chmod 644 /etc/systemd/system/puma.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable puma
+sudo systemctl start puma
