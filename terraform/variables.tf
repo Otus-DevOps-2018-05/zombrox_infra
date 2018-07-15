e55cf026c63fa004fa9dfb23f2f0e8a047856f5f
Oleg Klimin <oleg.v.klimin@gmail.com>
2018-07-16 00:23:50 +0300

    make main task

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable secret_key_path {
+  description = "Path to the secret key used for connection"
+}
+
+variable zone {
+  description = "Zone for instance"
+  default     = "europe-west1-d"
+}
